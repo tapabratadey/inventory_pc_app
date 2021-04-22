000acd2684df435a8b65b05e000ff9a2ae33340f
tapabratadey <tapabratadey02@gmail.com>
2021-04-21 22:25:34 -0400

    added cancel button functionality in controllers TODO 1) addPart 2) modifyPart 3) addProduct 4) modifyProduct 5) Search feature

--- src/controller/ModifyPart.java
+++ src/controller/ModifyPart.java
@@ -1,4 +1,38 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+
 public class ModifyPart {
+    public void modifyPartCancelButton(ActionEvent actionEvent) throws IOException {
+        Alert alertUser = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure?");
+        Optional<ButtonType> optButton = alertUser.showAndWait();
+        if (optButton.isPresent() && optButton.get() == ButtonType.OK){
+            //load widget hierarchy of next screen
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Main.fxml")));
+
+            //get the stage from an event's source widget
+            Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+            //Create the New Scene
+            Scene scene = new Scene(root, 1200, 600);
+            stage.setTitle("Inventory System");
+
+            //Set the Scene on the stage
+            stage.setScene(scene);
+
+            //raise the curtain
+            stage.show();
+        }
+    }
 }
